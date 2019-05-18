dbb2d1eb8ed3215a7287cea59eca81930bb9e631
Aninassimova <1402387648@qq.com>
2019-05-18 13:04:47 +0800

    第8章：回归

---
+++ ch08-Linear_regrs/__main__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from Build_model import *
+from Forward_stepReg import *
+from Local_weightLR import *
+from Price_predict import *
+from Ridge_regre import *
+from Stand_Linear import *
+
+if __name__ == '__main__':
+    lgX=[]; lgY=[]
+    
